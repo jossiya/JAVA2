7af4a7b1d24aec6c65634875eac1ef4612bff9c9
jossi <rolety2202@naver.com>
2022-08-23 19:30:24 +0900

    인스턴스

--- ProjectOOP/Print.java
+++ ProjectOOP/Print.java
@@ -1,15 +1,15 @@
 
 class Print {
-		public static String delimiter = "";
-		public static void A() {
+		public String delimiter = "";
+		public void A() {
 			System.out.println(delimiter);
 			System.out.println("A");
 			System.out.println("A");
 		}
 		
-		public static void B() {
+		public void B() {
 			System.out.println(delimiter);
-			System.out.println("A");
-			System.out.println("A");
+			System.out.println("B");
+			System.out.println("B");
 		}
 }
