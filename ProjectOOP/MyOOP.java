cf99b631732f9f239db1a8674f08e382a696df03
jossi <rolety2202@naver.com>
2022-08-23 19:30:14 +0900

    인스턴스

--- ProjectOOP/MyOOP.java
+++ ProjectOOP/MyOOP.java
@@ -1,17 +1,24 @@
 public class MyOOP {
 	//public 접근 제어자
 	public static void main(String[] args) {
-		Print.delimiter = "----";
-		Print.A();
-		Print.A();
-		Print.B();
-		Print.B();
+		Print p1=new Print();
+		p1.delimiter="----";
+		p1.A();
+		p1.A();
+		p1.B();
+		p1.B();
 		
-		Print.delimiter = "****";
-		Print.A();
-		Print.A();
-		Print.B();
-		Print.B();
+		Print p2=new Print();
+		p2.delimiter="****";
+		p2.A();
+		p2.A();
+		p2.B();
+		p2.B();
+		
+		p1.A();
+		p2.A();
+		p1.A();
+		p2.A();
 	}
 
 }
