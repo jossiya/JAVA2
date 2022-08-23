96d655906f9922a4b700120d44d09595bd3322f0
jossi <rolety2202@naver.com>
2022-08-23 20:07:04 +0900

    생성자와 this

--- ProjectOOP/MyOOP.java
+++ ProjectOOP/MyOOP.java
@@ -1,15 +1,13 @@
 public class MyOOP {
 	//public 접근 제어자
 	public static void main(String[] args) {
-		Print p1=new Print();
-		p1.delimiter="----";
+		Print p1=new Print("----");
 		p1.A();
 		p1.A();
 		p1.B();
 		p1.B();
 		
-		Print p2=new Print();
-		p2.delimiter="****";
+		Print p2=new Print("****");
 		p2.A();
 		p2.A();
 		p2.B();
