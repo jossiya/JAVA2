afb3bf8c982a276489a11a11405cc6cd4d2863e4
jossi <rolety2202@naver.com>
2022-08-23 18:07:04 +0900

    전역 변수

---
+++ ProjectOOP/MyOOP.java
@@ -0,0 +1,30 @@
+
+public class MyOOP {
+	
+	public static void main(String[] args) {
+		delimiter = "----";
+		printA(delimiter);
+		printA(delimiter);
+		printB(delimiter);
+		printB(delimiter);
+		
+		delimiter = "****";
+		printA(delimiter);
+		printA(delimiter);
+		printB(delimiter);
+		printB(delimiter);
+	}
+	public static String delimiter = "";
+	public static void printA(String delimiter) {
+		System.out.println(delimiter);
+		System.out.println("A");
+		System.out.println("A");
+	}
+	
+	public static void printB(String delimiter) {
+		System.out.println(delimiter);
+		System.out.println("A");
+		System.out.println("A");
+	}
+
+}
