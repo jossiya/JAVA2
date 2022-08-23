632091a7437cf59146429761c64c8caf57f3cb2b
jossi <rolety2202@naver.com>
2022-08-23 20:06:46 +0900

    생성자와 this

--- ProjectOOP/Print.java
+++ ProjectOOP/Print.java
@@ -1,14 +1,17 @@
 
 class Print {
 		public String delimiter = "";
+		public Print(String delimiter) {//클레스와 이름이 메서드로 정의-> 생성자.
+			this.delimiter=delimiter;
+		}
 		public void A() {
-			System.out.println(delimiter);
+			System.out.println(this.delimiter);
 			System.out.println("A");
 			System.out.println("A");
 		}
 		
 		public void B() {
-			System.out.println(delimiter);
+			System.out.println(this.delimiter);
 			System.out.println("B");
 			System.out.println("B");
 		}
