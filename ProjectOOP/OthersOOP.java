890b49a8266df72e41deeebf8588c78fd9ce10cf
jossi <rolety2202@naver.com>
2022-08-23 17:56:50 +0900

    남의 클레스 인스턴스 쓰기

--- ProjectOOP/OthersOOP.java
+++ ProjectOOP/OthersOOP.java
@@ -1,9 +1,27 @@
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class OthersOOP {
-
-	public static void main(String[] args) {
+//class : System , Math, FileWriter
+//Instance : f1 , f2 
+	public static void main(String[] args) throws IOException {
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(1.8));
+		System.out.println(Math.ceil(1.8));
 		
-
+		FileWriter f1=new FileWriter("data.txt");
+		f1.write("Hello");
+		f1.write("Java");
+		
+		FileWriter f2=new FileWriter("data2.txt");
+		f2.write("Hello");
+		f2.write("Java2");
+		f2.close();
+		
+		
+		f1.write("!!!!");
+		f1.close();
+		
 	}
 
 }
